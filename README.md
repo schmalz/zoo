32dd7c438e24770c867d3068d4ef573972c3c413
schmalz <schmalz@live.co.uk>
2023-03-27 14:56:23 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,36 @@
+# Zoo
+
+## Introduction
+
+A while ago I was given a specification that went something like:
+
+--- 8< ---
+
+You have been asked to write a part of a simulation of a small zoo.
+
+The zoo allows dogs, cats and mice into it and only accepts aminals with unique names.
+
+The different species of animal have differing satisfaction levels when being fed:
+
+ - Dogs: 35
+ - Cats: 25
+ - Mice 10
+
+And have different calls:
+
+ - Dogs: "woof"
+ - Cats: "meow"
+ - Mice "eek"
+
+ The animals in the zoo are fed once a day; on Tuesday and Thursday they are fed 40 and on all other days 20. When they are fed, each animal responds with its call, lower-case if it is satisfied, upper-case if not.
+
+ On weekdays the animals are fed in alphabetical order of their names, on weekends they are fed in any order.
+
+ You should include some tests to show the simulation working.
+
+ --- 8< ---
+
+ ## Solution
+
+ Several solutions were submitted, this is mine. :)
+
---
+++ src/zoo/core.clj
@@ -0,0 +1,98 @@
+(ns zoo.core
+  (:require [clojure.string :refer [upper-case]]))
+
+(def ^:private species-keywords
+  {"dog" :dog
+   "cat" :cat
+   "mouse" :mouse})
+
+(def ^:private calls
+  {:dog "woof"
+   :cat "meow"
+   :mouse "eek"})
+
+(def ^:private satisfaction-levels
+  {:dog 35
+   :cat 25
+   :mouse 10})
+
+(def ^:private day-keywords
+  {"monday" :monday
+   "tuesday" :tuesday
+   "wednesday" :wednesday
+   "thursday" :thursday
+   "friday" :friday
+   "saturday" :saturday
+   "sunday" :sunday})
+
+(def ^:private feed-levels
+  {:monday 40
+   :tuesday 20
+   :wednesday 40
+   :thursday 20
+   :friday 40
+   :saturday 40
+   :sunday 40})
+
+(def ^:private zoo
+  (atom (sorted-map)))
+
+(defn- make-animal
+  [species name]
+  (let [species-key (species-keywords species)]
+    {:species species-key
+     :name name
+     :call (calls species-key)
+     :satisfaction-level (satisfaction-levels species-key)}))
+
+(defn- feed
+  [animal amount]
+  (let [call (animal :call)]
+    (if (<= amount (animal :satisfaction-level))
+      call
+      (upper-case call))))
+
+(defn add-animal
+  "Add an animal called NAME of SPECIES to the zoo. Returns the value of zoo
+   if the animal was successfully added, FALSE otherwise (an animal with NAME
+   already exists)."
+  [species name]
+  (letfn [(add [zoo]
+            (if (not (contains? zoo name))
+              (assoc zoo name (make-animal species name))
+              (throw (IllegalStateException. "name already exists in zoo"))))]
+    (try
+      (swap! zoo add)
+      (catch Exception _ex
+        false))))
+
+(defn feed-aminals
+  "Feed all the animals in the zoo on DAY."
+  [day]
+  (let [day-key (day-keywords day)]
+    (map (fn [animal]
+           (feed animal (feed-levels day-key)))
+         (vals @zoo))))
+
+;; Tests.
+
+(assert (add-animal "cat" "felix")
+        "felix the cat added to the zoo")
+
+(assert (not (add-animal "cat" "felix"))
+        "felix the cat not added to the zoo")
+
+(assert (add-animal "cat" "tom")
+        "tom the cat added to the zoo")
+
+(assert (add-animal "mouse" "jerry")
+        "jerry the mouse added to the zoo")
+
+(assert (add-animal "dog" "fido")
+        "fido the dog added to the zoo")
+
+(assert (= ["MEOW" "WOOF" "EEK" "MEOW"]
+           (feed-aminals "monday")))
+
+(assert (= ["meow" "woof" "EEK" "meow"]
+           (feed-aminals "tuesday")))
